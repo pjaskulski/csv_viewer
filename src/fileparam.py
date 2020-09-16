a06aebf77689acc8bc6a97f45c516fd1d1ed9304
piotr.jaskulski@gmail.com <piotr.jaskulski@gmail.com>
2020-09-16 08:55:11 +0200

    preview widget in open dialog

--- src/fileparam.py
+++ src/fileparam.py
@@ -1,6 +1,8 @@
+import os
 from PyQt5 import QtCore
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QDialog, QMessageBox, QDialogButtonBox, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, \
-    QPushButton, QRadioButton, QFileDialog, QGroupBox, QSpacerItem
+    QPushButton, QRadioButton, QFileDialog, QGroupBox, QPlainTextEdit
 
 
 class ParameterDialog(QDialog):
@@ -13,6 +15,11 @@ class ParameterDialog(QDialog):
         self.buttonBox = QDialogButtonBox(QBtn)
         self.buttonBox.accepted.connect(self.validate)
         self.buttonBox.rejected.connect(self.reject)
+        self.preview = QPlainTextEdit()
+        # set font to monospaced
+        font = self.preview.font()
+        font.setFamily("Courier New")
+        self.preview.setFont(font)
 
         self.layout = QVBoxLayout()
 
@@ -23,6 +30,7 @@ class ParameterDialog(QDialog):
 
         self.layout_file.addWidget(QLabel("File:"))
         self.filename = QLineEdit()
+        self.filename.textChanged.connect(self.onFileNameTextChange)
         self.filename.setText(file_name)
         self.layout_file.addWidget(self.filename)
         self.btn_file = QPushButton("...")
@@ -80,11 +88,32 @@ class ParameterDialog(QDialog):
         self.layout_dp.addWidget(self.radio_dp_dot)
         self.layout_dp.addWidget(self.radio_dp_comma)
         #
-
         self.layout.addLayout(self.layout_sepdec)
+
+        # preview csv file
+        groupbox_pre = QGroupBox("Preview:")
+        self.layout.addWidget(groupbox_pre)
+        self.layout_pre = QHBoxLayout()
+        groupbox_pre.setLayout(self.layout_pre)
+
+        file_head = self.file_preview(self.filename.text())
+        self.preview.document().setPlainText(file_head)
+        self.preview.setReadOnly(True)
+        self.layout_pre.addWidget(self.preview)
+
         self.layout.addWidget(self.buttonBox)
 
         self.setLayout(self.layout)
+
+    def file_preview(self, path: str, n=8) -> str:
+        """ return first n lines from csv file, if file exists """
+        file_head = ''
+        if path != '' and os.path.isfile(path):
+            with open(path, 'r') as f:
+                for i in range(1, n):
+                    file_head += f.readline()
+
+        return file_head
 
     def validate(self):
         if self.filename.text() == '':
@@ -108,3 +137,10 @@ class ParameterDialog(QDialog):
             self, "Open CSV file...", "", "CSV (*.csv);;All Files (*)")
         if file_name:
             self.filename.setText(file_name)
+            file_head = self.file_preview(self.filename.text())
+            self.preview.document().setPlainText(file_head)
+
+    def onFileNameTextChange(self):
+        """ on text change in field filename (QLineEdit) - clear and set new preview"""
+        file_head = self.file_preview(self.filename.text())
+        self.preview.document().setPlainText(file_head)
